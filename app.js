89c3acfb63b3c3dc940429ac5ba47495ec51896f
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-10 10:17:09 -1000

    removed unnecessary inner function

--- app.js
+++ app.js
@@ -3,23 +3,20 @@ let emailLogData = function () {
 
   let emailLog = require("./emails.json");
 
-  function makeEmailCountObj() {
-    const emailAddressCounts = emailLog.emails.reduce(function (prevVal, curVal) {
-      if (prevVal.hasOwnProperty(curVal.email)) {
-        prevVal[curVal.email] += 1;
-      }
-      if (!prevVal.hasOwnProperty(curVal.email)) {
-        prevVal[curVal.email] = 1;
-      }
-      return prevVal;
-    }, {});
-    return emailAddressCounts;
-  }
+  const emailAddressCounts = emailLog.emails.reduce(function (prevVal, curVal) {
+    if (prevVal.hasOwnProperty(curVal.email)) {
+      prevVal[curVal.email] += 1;
+    }
+    if (!prevVal.hasOwnProperty(curVal.email)) {
+      prevVal[curVal.email] = 1;
+    }
+    return prevVal;
+  }, {});
 
   return {
-    makeEmailCountObj: makeEmailCountObj
+    emailAddressCounts: emailAddressCounts
   }
 }
 
 const emailModule = emailLogData();
-console.log(emailModule.makeEmailCountObj());
+console.log(emailModule.emailAddressCounts);

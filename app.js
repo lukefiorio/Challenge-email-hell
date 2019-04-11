d2cb97fa2146e2a05cd2c8ea75f3dd86427ba43e
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-10 15:02:17 -1000

    strict mode + code formatting

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+'use strict';
 
 let emailLogData = function () {
-
-  let emailLog = require("./emails.json");
+  let emailLog = require('./emails.json');
 
   const emailAddressCounts = emailLog.emails.reduce(function (prevVal, curVal) {
     if (prevVal.hasOwnProperty(curVal.email)) {
       prevVal[curVal.email] += 1;
-    }
-    if (!prevVal.hasOwnProperty(curVal.email)) {
+    } else if (!prevVal.hasOwnProperty(curVal.email)) {
       prevVal[curVal.email] = 1;
     }
     return prevVal;
